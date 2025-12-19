ddd14779ee5b79a418cfb9f9b24feedf391d4c50
winter3671 <wjdgns9805@naver.com>
2025-12-19 11:47:00 +0900

    feat: 조회용 API 기능 추가

--- back/trips/views.py
+++ back/trips/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Trip
+from .serializers import TripListSerializer, TripDetailSerializer
 
-# Create your views here.
+class TripListView(generics.ListAPIView):
+    queryset = Trip.objects.filter(status='active').order_by('-recommendation_score')
+    serializer_class = TripListSerializer
+
+class TripDetailView(generics.RetrieveAPIView):
+    queryset = Trip.objects.filter(status='active')
+    serializer_class = TripDetailSerializer
