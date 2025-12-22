7d2049f1e8aca5f853aa7630f31c3a57f82b6cc6
KnD0715 <rlaalswo0715@naver.com>
2025-12-22 11:21:50 +0900

    feat: 회원가입 구현

--- back/users/serializers.py
+++ back/users/serializers.py
@@ -3,18 +3,8 @@ from dj_rest_auth.registration.serializers import RegisterSerializer
 from .models import User
 
 class CustomRegisterSerializer(RegisterSerializer):
-    name = serializers.CharField(max_length=100)
-    phone = serializers.CharField(max_length=20)
-
-    def get_cleaned_data(self):
-        data = super().get_cleaned_data()
-        data['name'] = self.validated_data.get('name', '')
-        data['phone'] = self.validated_data.get('phone', '')
-        return data
-
+    email = serializers.EmailField(required=False, allow_blank=True)
+    
     def save(self, request):
         user = super().save(request)
-        user.name = self.cleaned_data.get('name')
-        user.phone = self.cleaned_data.get('phone')
-        user.save()
         return user
---
+++ front/final_project/src/stores/accounts.js
@@ -0,0 +1,72 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+
+export const useAccountStore = defineStore('account', () => {
+    const router = useRouter()
+    const token = ref(null)
+
+    const API_URL = 'http://127.0.0.1:8000'
+
+    const signUp = function(payload) {
+        const username = payload.username
+        const email = payload.email
+        const password1 = payload.password1
+        const password2 = payload.password2
+
+        const requestData = {
+            username,
+            password1,
+            password2
+        }
+
+        if (email && email.trim() !== '') {
+            requestData.email = email
+        }
+        axios ({
+            method: 'post',
+            url: `${API_URL}/api/auth/registration/`,
+            data: requestData
+        }).then(res => {
+            console.log('회원 가입이 완료되었습니다.')
+            const password = password1
+            logIn({ username, password })
+        }).catch(err => console.log(err))
+    }
+
+    const logIn = function (payload) {
+        const username = payload.username
+        const password = payload.password
+
+        axios({
+            method: 'post',
+            url: `${API_URL}/api/auth/login`,
+            data: {
+                username, password
+            }
+        }).then(res => {
+            token.value = res.data.access
+            router.push({ name : 'home'})
+        }).catch(err => console.log(err))
+    }
+
+    const isLogin = computed(() => {
+        return token.value ? true : false
+    })
+
+     const logOut = function () {
+        token.value = null
+        router.push({ name : 'login'})
+    }
+
+
+    return {
+        signUp,
+        logIn,
+        isLogin,
+        logOut,
+        token
+
+    }
+}, { persist: true })
